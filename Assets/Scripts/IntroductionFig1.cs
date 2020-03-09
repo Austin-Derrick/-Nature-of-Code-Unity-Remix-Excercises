b79623b4c74a46fbfcdb823a9a35c5d4d3d449ae
Austin-Derrick <aderrickson@colum.edu>
2020-03-08 20:56:15 -0500

    Added scenes and scripts for Introduction Figures 1 & 2

---
+++ Assets/Scripts/IntroductionFig1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroductionFig1 : MonoBehaviour
+{
+    public GameObject walkerPrefab;
+
+    private GameObject walkerGO;
+
+    private Walker walker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject walkerGameObject = new GameObject();
+
+        walker = walkerGameObject.AddComponent<Walker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walker.Step();
+
+        walker.draw();
+    }
+}
+
+public class Walker : MonoBehaviour
+{
+    GameObject walkerGO;
+
+    float x;
+    float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkerGO = this.gameObject;
+
+        x = 0;
+        y = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Step()
+    {
+        float stepx = Random.Range(0, 4) - 1;
+        float stepy = Random.Range(0, 4) - 1;
+
+        x += stepx;
+        y += stepy;
+
+        walkerGO.transform.position = new Vector3(x, y, 0F);
+    }
+
+    public void draw()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        sphere.transform.position = new Vector3(walkerGO.transform.position.x, walkerGO.transform.position.y, 0f);
+    }
+}
